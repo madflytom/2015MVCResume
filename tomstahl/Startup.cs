eb13788373056fa5f562600d8b186b58d600c4eb
madflytom <madflytom@gmail.com>
2015-02-22 07:02:51 -0500

    Initial commit

---
+++ tomstahl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tomstahl.Startup))]
+namespace tomstahl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
